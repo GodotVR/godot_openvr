00e5225dc8cd5a621f41d6c380f70856a73e4e88
Bastiaan Olij <mux213@gmail.com>
2017-12-20 23:02:14 +1100

    Improved controller scene and added example FPS scene

--- demo/Main.gd
+++ demo/Main.gd
@@ -5,14 +5,9 @@ func _ready():
 	var arvr_interface = ARVRServer.find_interface("OpenVR")
 	if arvr_interface and arvr_interface.initialize():
 		get_viewport().arvr = true
-
+		
 		# workaround in OpenVR because OpenVR does not like our HDR buffers, so turn it off for now...
 		get_viewport().hdr = false
-		
-		# No longer needed, will remove soon. 
-		# resize our window so we see a smaller preview of our left eye
-		# var size = arvr_interface.get_render_targetsize() / 3.0
-		# OS.set_window_size(size);
 	
 	# just for testing, list what models are available
 	var ovr_model = preload("res://bin/OpenVRRenderModel.gdns").new()
@@ -26,15 +21,3 @@ func _process(delta):
 	elif (Input.is_key_pressed(KEY_SPACE)):
 		# Calling center_on_hmd will cause the ARVRServer to adjust all tracking data so the player is centered on the origin point looking forward
 		ARVRServer.center_on_hmd(true, true)
-
-	# We minipulate our origin point to move around. Note that with roomscale tracking a little more then this is needed
-	# because we'll rotate around our origin point, not around our player. But that is a subject for another day.
-	if (Input.is_key_pressed(KEY_LEFT)):
-		$ARVROrigin.rotation.y += delta
-	elif (Input.is_key_pressed(KEY_RIGHT)):
-		$ARVROrigin.rotation.y -= delta
-
-	if (Input.is_key_pressed(KEY_UP)):
-		$ARVROrigin.translation -= $ARVROrigin.transform.basis.z * delta;
-	elif (Input.is_key_pressed(KEY_DOWN)):
-		$ARVROrigin.translation += $ARVROrigin.transform.basis.z * delta;
