60c804c8a509d0ed18aa230e7fcdf6aa14881ae5
bruvzg <7645683+bruvzg@users.noreply.github.com>
2018-03-17 01:45:24 +0200

    Force redefined GDNative structs to be classified as INTEGER by System V AMD64 ABI.

--- src/GodotCalls.h
+++ src/GodotCalls.h
@@ -5,11 +5,17 @@
 #ifndef GODOT_CALLS_H
 #define GODOT_CALLS_H
 
+#include <stdint.h>
+
 // fully define these, don't waste time with needless callbacks for access
 #define GODOT_CORE_API_GODOT_VECTOR2_TYPE_DEFINED
-typedef struct {
-	float x;
-	float y;
+typedef union {
+	//Force struct to be classified as INTEGER by System V AMD64 ABI.
+	uint8_t _dont_touch_that[8];
+	struct {
+		float x;
+		float y;
+	};
 
 	inline void set(float p_x, float p_y) {
 		x = p_x;
@@ -18,10 +24,14 @@ typedef struct {
 } godot_vector2;
 
 #define GODOT_CORE_API_GODOT_VECTOR3_TYPE_DEFINED
-typedef struct {
-	float x;
-	float y;
-	float z;
+typedef union {
+	//Force struct to be classified as INTEGER by System V AMD64 ABI.
+	uint8_t _dont_touch_that[12];
+	struct {
+		float x;
+		float y;
+		float z;
+	};
 
 	inline void set(float p_x, float p_y, float p_z) {
 		x = p_x;
@@ -31,14 +41,17 @@ typedef struct {
 } godot_vector3;
 
 #define GODOT_CORE_API_GODOT_RECT2_TYPE_DEFINED
-typedef struct {
-	godot_vector2 position;
-	godot_vector2 size;
+typedef union {
+	//Force struct to be classified as INTEGER by System V AMD64 ABI.
+	uint8_t _dont_touch_that[16];
+	struct {
+		godot_vector2 position;
+		godot_vector2 size;
+	};
 } godot_rect2;
 
 #include <gdnative_api_struct.gen.h>
 
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
