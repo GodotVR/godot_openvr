cab2af33ae4a49787ffc45af3cbb2ee3945e7f80
Nidonocu <nidonocu@gmail.com>
2021-03-13 23:38:32 +0000

    More bug fix!

--- src/open_vr/OpenVRFocus.cpp
+++ src/open_vr/OpenVRFocus.cpp
@@ -7,21 +7,22 @@ void OpenVRFocus::_register_methods() {
 
 	register_method("get_is_dashboard_active", &OpenVRFocus::get_is_dashboard_active);
 
-	register_property<OpenVRFocus, bool>("use_auto_pause", &OpenVRFocus::set_auto_pause_on_dashboard, &OpenVRFocus::get_auto_pause_on_dashboard, bool);
+	register_property<OpenVRFocus, bool>("use_auto_pause", &OpenVRFocus::set_auto_pause_on_dashboard, &OpenVRFocus::get_auto_pause_on_dashboard, false);
 	
 	register_signal<OpenVRFocus>(String("dashboard_opened"), Dictionary());
 	register_signal<OpenVRFocus>(String("dashboard_closed"), Dictionary());
 }
 
 void OpenVRFocus::_init() {
+	dashboard_triggers_pause = false;
 }
 
 void OpenVRFocus::_process(float delta) {
     if (ovr != NULL) {
-        bool is_dashboard_active_current = ovr->is_dashboard_active;
+        bool is_dashboard_active_current = ovr->is_dashboard_active();
         if (is_dashboard_active != is_dashboard_active_current) {
             is_dashboard_active = is_dashboard_active_current;
-            if (is_action_set_active) {
+            if (is_dashboard_active) {
                 emit_signal("dashboard_opened");
 				if (dashboard_triggers_pause) {
 					get_tree().paused = true;
@@ -48,6 +49,7 @@ void OpenVRFocus::set_auto_pause_on_dashboard(bool p_auto) {
 
 OpenVRFocus::OpenVRFocus() {
 	ovr = openvr_data::retain_singleton();
+	is_dashboard_active = false;
 }
 
 OpenVRFocus::~OpenVRFocus() {
