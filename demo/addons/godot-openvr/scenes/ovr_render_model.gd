6a3200b109c7372c23ee47fa66eefecc2b21b9b6
Bill Doyle <bdoyle@liminetics.com>
2025-10-08 12:23:17 -0400

    Make ovr_render_model.gd actually work again
    Use signals from XRServer to trigger reloading render models from OpenVR
    when needed. Also removed the generic_controller fallback, as we don't
    currently have a way to actually signal failure from the deferred
    loading in openvr_data.

--- demo/addons/godot-openvr/scenes/ovr_render_model.gd
+++ demo/addons/godot-openvr/scenes/ovr_render_model.gd
@@ -5,69 +5,69 @@ class_name OVRRenderModel3D
 var ovr: XRInterfaceOpenVR
 
 var ws = 0
-var controller_is_loaded = false
+
+## Keep track of which render model is already loaded so we don't flicker by
+## replacing it with the same one if tracker signals are received but the
+## controller didn't actually change.
+var _loaded_render_model_name: String = ""
+
+var _xr_tracker:
+	get:
+		var parent = get_parent()
+		if parent is XRController3D:
+			return parent.tracker
+
+		return ""
+
 
 ## Override the render model. If unset and the parent of this node is an XRController3D,
 ## an attempt will be made to locate the correct model automatically.
 @export var render_model_name: String:
+	get:
+		if render_model_name:
+			return render_model_name
+		elif _xr_tracker:
+			# The openvr interface sets the tracker description to the render model id.
+			var tracker = XRServer.get_tracker(_xr_tracker)
+			if tracker:
+				return tracker.description
+
+		return ""
+
 	set(name):
 		render_model_name = name
-		controller_is_loaded = false
+		update_rendermodel()
 
-func apply_world_scale():
+
+func _enter_tree():
+	ovr = OpenVRInterface.get_interface()
+
+	XRServer.tracker_added.connect(_on_tracker_changed, CONNECT_DEFERRED)
+	XRServer.tracker_updated.connect(_on_tracker_changed, CONNECT_DEFERRED)
+	XRServer.tracker_removed.connect(_on_tracker_changed, CONNECT_DEFERRED)
+
+
+func _ready():
+	update_rendermodel()
+
+
+func _physics_process(_delta):
 	var new_ws = XRServer.world_scale
 	if (ws != new_ws):
 		ws = new_ws
 		scale = Vector3(ws, ws, ws)
-#
-# Called when the node enters the scene tree for the first time.
-func _enter_tree():
-	ovr = OpenVRInterface.get_interface()
 
-func _load_controller_mesh(controller_name):
-	# TODO: This can never actually return false, the fallback is useless.
-	var model: ArrayMesh
-	model = ovr.load_render_model(controller_name)
-	if model:
-		return model
 
-	model = ovr.load_render_model("generic_controller")
-	if model:
-		return model
+func _on_tracker_changed(tracker_name, _type):
+	print_debug("tracker changed: " + tracker_name)
+	if tracker_name == _xr_tracker:
+		update_rendermodel()
 
-	return Mesh.new()
 
-func _process(_delta):
-	var controller: XRController3D
-	var model_name = render_model_name
-
-	if not render_model_name:
-		# TODO: Actually do the autodetection, this doesn't work
-		if not get_parent() is XRController3D:
-			print("Can't detect render model, please specify one")
-			return
-
-		controller = get_parent()
-
-		if controller and !controller.get_is_active():
-			if controller_is_loaded:
-				mesh = Mesh.new()
-				controller_is_loaded = false
-			return
-
-		# always set our world scale, user may end up changing this
-		apply_world_scale()
-
-		if controller_is_loaded:
-			return
-
-		# became active? lets handle it...
-		var controller_name = controller.get_controller_name()
-		print("Controller " + controller_name + " became active")
-		model_name = controller_name.substr(0, controller_name.length()-2)  # XXX: why?
-
-	# attempt to load a mesh for this
-	# TODO: can't tell if this actually worked, need a way to reset if the load function returns an error later on in the render thread
-	if not controller_is_loaded:
-		mesh = _load_controller_mesh(model_name)
-		controller_is_loaded = true
+func update_rendermodel():
+	if render_model_name and render_model_name != _loaded_render_model_name:
+		mesh = ovr.load_render_model(render_model_name)
+		_loaded_render_model_name = render_model_name
+	else:
+		mesh = null
+		_loaded_render_model_name = ""
